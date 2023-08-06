122e448cbbaef2fb1f2004ba2b8c756c7e02bb33
Anna Wójcik <aniawojcik894@gmail.com>
2023-08-06 20:28:53 +0200

    js rebuilding - arrow functions added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
-console.log("Hello!");
+{
+    const welcome = () => {
+        console.log("Hello!");
+    };
 
-let buttonChangingTheme = document.querySelector(".js-header__button");
-let themeDark = document.querySelector(".themeDark");
-let themeName = document.querySelector(".js-header__button--themeName");
-let colorBody = document.querySelector(".js-body");
-let colorOfHeader = document.querySelector(".js-header");
-let oMnieDark = document.querySelector(".js-navigation__oMnie--dark");
-let zainteresowaniaDark = document.querySelector(".js-navigation__zainteresowania--dark");
-let kontaktDark = document.querySelector(".js-navigation__kontakt--dark");
-let sectionOMnieFontBright = document.querySelector(".js-section__oMnie--font-bright");
-let sectionZainteresowaniaFontBright = document.querySelector(".js-section__zainteresowania--font-bright");
-let sectionKontaktFontBright = document.querySelector(".js-section__kontakt--font-bright");
+    const toggleBackground = () => {
+        const colorBody = document.querySelector(".js-body");
+        const colorOfHeader = document.querySelector(".js-header");
+        const oMnieDark = document.querySelector(".js-navigation__oMnie--dark");
+        const zainteresowaniaDark = document.querySelector(".js-navigation__zainteresowania--dark");
+        const kontaktDark = document.querySelector(".js-navigation__kontakt--dark");
+        const sectionOMnieFontBright = document.querySelector(".js-section__oMnie--font-bright");
+        const sectionZainteresowaniaFontBright = document.querySelector(".js-section__zainteresowania--font-bright");
+        const sectionKontaktFontBright = document.querySelector(".js-section__kontakt--font-bright");
+        const themeName = document.querySelector(".js-header__button--themeName");
 
+        colorBody.classList.toggle("themeDark");
+        colorOfHeader.classList.toggle("themeDark");
+        oMnieDark.classList.toggle("navigation__item--font-bright");
+        zainteresowaniaDark.classList.toggle("navigation__item--font-bright");
+        kontaktDark.classList.toggle("navigation__item--font-bright");
+        sectionOMnieFontBright.classList.toggle("section--font-bright");
+        sectionZainteresowaniaFontBright.classList.toggle("section--font-bright");
+        sectionKontaktFontBright.classList.toggle("section--font-bright");
 
+        if (colorBody.classList.contains("themeDark")) {
+            themeName.innerText = "jasny";
+        } else {
+            themeName.innerText = "ciemny";
+        }
+    };
 
-buttonChangingTheme.addEventListener("click", () => {
-    colorBody.classList.toggle("themeDark"); // this instruction changes the class of body
-    colorOfHeader.classList.toggle("themeDark");
-    oMnieDark.classList.toggle("navigation__item--font-bright");
-    zainteresowaniaDark.classList.toggle("navigation__item--font-bright");
-    kontaktDark.classList.toggle("navigation__item--font-bright");
-    sectionOMnieFontBright.classList.toggle("section--font-bright");
-    sectionZainteresowaniaFontBright.classList.toggle("section--font-bright");
-    sectionKontaktFontBright.classList.toggle("section--font-bright");
+    const init = () => {
+        const buttonChangingTheme = document.querySelector(".js-header__button");
+        buttonChangingTheme.addEventListener("click", toggleBackground);
 
-    if (colorBody.classList.contains("themeDark")) {
-        themeName.innerText = "jasny";
-    } else {
-        themeName.innerText = "ciemny";
-    }
-})
+        welcome();
+    };
 
-
+    init();
+}
